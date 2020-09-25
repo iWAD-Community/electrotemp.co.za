92737246541d3eba16bee63547f26409acea581a
witnesstakarinda <38228469+witnesstakarinda@users.noreply.github.com>
2020-09-25 15:12:56 +0200

    First Commit
    Uploading Version 1.0 to github

---
+++ js/Repairs.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){ 
+
+    $(document).ready(function(){
+        $(".btnRepair").click(function(event){
+            
+            var statusMessage = $('.messgageSent')
+            statusMessage.append("Our highly qualified technicians who are proud to guarantee their workmanship will offer a professional advice in all your Air-conditioning and Refrigeration needs.")
+            event.preventDefault()
+        })
+    })
+    
+
+})
